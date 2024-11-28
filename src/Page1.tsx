e65263bc1e632e17ca5d9d646456846870813d47
T_Mori <morit0301@gmail.com>
2024-11-28 19:33:00 +0900

    Page transitions that pass state

--- src/Page1.tsx
+++ src/Page1.tsx
@@ -1,13 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Page 1</h1>
       <nav>
         <ul>
           <li>
-            <Link to="detailA">Detail A</Link>
+            {/* <button onClick={() => navigate("detailA", { state: arr })}>
+              Detail A
+            </button> */}
+            <Link to="detailA" state={arr}>
+              Detail A
+            </Link>
           </li>
           <li>
             <Link to="detailB">Detail B</Link>
